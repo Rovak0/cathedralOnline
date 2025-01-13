3c191bb92ed5b7120cfa0b648541662f22b5943c
Rovak0 <ericjackwong@outlook.com>
2025-01-13 12:18:01 -0500

    hunting sqlize

--- config/connection.js
+++ config/connection.js
@@ -10,7 +10,14 @@ const DB_NAME = "chess_db"
 
 if (process.env.Internal_Database_URL) {
   sequelize = new Sequelize(process.env.Internal_Database_URL);
-  console.log("FIND ME: " + sequelize);
+  try{
+    await sequelize.authenticate();
+    console.log("FIND ME: Pass sequilize");
+
+  }
+  catch(err){
+    console.log("FIND ME: Failed sequilize");
+  }
 }
 // else if(process.env.External_Database_URL){
 //   console.log("Outside base");
