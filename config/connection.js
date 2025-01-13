e115dee44e96a11a41ecf0204d86bed97fcaa5e3
Rovak0 <ericjackwong@outlook.com>
2025-01-13 10:34:13 -0500

    env working

--- config/connection.js
+++ config/connection.js
@@ -12,9 +12,12 @@ if (process.env.DB_URL) {
   sequelize = new Sequelize(process.env.DB_URL);
 } else {
   sequelize = new Sequelize(
-    DB_NAME,
-    DB_USER,
-    DB_PASSWORD,
+    // DB_NAME,
+    // DB_USER,
+    // DB_PASSWORD,
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
     {
       host: 'localhost',
       dialect: 'postgres'
